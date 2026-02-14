190850e80591cb882afa5ba09c31fbd3f9dedf48
donmaria <2006donmaria@gmail.com>
2026-02-14 08:57:05 +0530

    final commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// Serve frontend files
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+app.use("/api/auth", require("./routes/auth"));
+
+app.listen(5000, () => {
+    console.log("Server running on http://localhost:5000");
+});
